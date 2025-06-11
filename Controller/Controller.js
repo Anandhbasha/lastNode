1db887fb6e235e5dd46c8ed4d384c3e10b80bd43
Anandhbasha <anadhakm@gmail.com>
2025-06-11 10:47:47 +0530

    read insert

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,9 +1,25 @@
-export const read_data = (req,res)=>{
-    res.send("Node is working")
+import User from "../Model/Model.js"
+
+export const read_data = async(req,res)=>{
+    try {
+        const read = await User.find()
+        res.status(200).json(read)
+    } catch (error) {
+       res.status(402).json({message:"Unable to read Datas"}) 
+    }
 }
-export const insert_data = (req,res)=>{
-    const {userName,userPassword} = req.body
-    res.send(`UserName is :${userName} and userPasword is: ${userPassword}`)
+export const insert_data = async(req,res)=>{
+    try {
+        const {userName,userMobile} = req.body
+        const exists = await User.findOne({userMobile})
+        if(exists){
+             return res.status(405).json({message:"User Already exist"})
+        }
+        const insert = await User({userName,userMobile}).save()
+        return res.status(202).json({message:"Data Inserted Suceesfully",insert})
+    } catch (error) {
+        res.status(403).json({message:"Unable to insert Data"})
+    }
 }
 export const updateData = (req,res)=>{
     const {id} = req.params
